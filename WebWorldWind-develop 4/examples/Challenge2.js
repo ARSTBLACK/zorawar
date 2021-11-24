4e386bfdf44d82527935f2b96224486ac2aca740
Ianyliu <ian.liu@g.northernacademy.org>
2021-11-24 16:55:29 -0500

    MY TOTAL BRAIN CELLS WENT FROM 4 TO 3 TODAY.

--- WebWorldWind-develop 4/examples/Challenge2.js
+++ WebWorldWind-develop 4/examples/Challenge2.js
@@ -1,57 +1,4 @@
 /*
- * Copyright 2003-2006, 2009, 2017, United States Government, as represented by the Administrator of the
- * National Aeronautics and Space Administration. All rights reserved.
- *
- * The NASAWorldWind/WebWorldWind platform is licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * Illustrates how to display and pick Placemarks.
- */
-requirejs(['./WorldWindShim',
-        './LayerManager'],
-    function (WorldWind,
-              LayerManager) {
-        "use strict";
-
-        // Tell WorldWind to log only warnings and errors.
-        WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
-
-        // Create the WorldWindow.
-        var wwd = new WorldWind.WorldWindow("canvasOne");
-
-        // Create and add layers to the WorldWindow.
-        var layers = [
-            // Imagery layers.
-            {layer: new WorldWind.BMNGLayer(), enabled: true},
-            {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
-            {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
-            // Add atmosphere layer on top of all base layers.
-            {layer: new WorldWind.AtmosphereLayer(), enabled: true},
-            // WorldWindow UI layers.
-            {layer: new WorldWind.CompassLayer(), enabled: true},
-            {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
-            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
-        ];
-
-        for (var l = 0; l < layers.length; l++) {
-            layers[l].layer.enabled = layers[l].enabled;
-            wwd.addLayer(layers[l].layer);
-        }
-
-        // Define the images we'll use for the placemarks.
-        var images = ["Bean.png"
-        ];
-        /*
          * Copyright 2003-2006, 2009, 2017, 2020 United States Government, as represented
          * by the Administrator of the National Aeronautics and Space Administration.
          * All rights reserved.
@@ -107,9 +54,9 @@ requirejs(['./WorldWindShim',
                     {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
                 ];
 
-                for (var l = 0; l < layers.length; l++) {
-                    layers[l].layer.enabled = layers[l].enabled;
-                    wwd.addLayer(layers[l].layer);
+                for (var b = 0; b < layers.length; b++) {
+                    layers[b].layer.enabled = layers[b].enabled;
+                    wwd.addLayer(layers[b].layer);
                 }
 
                 // Define the images we'll use for the placemarks.
@@ -230,28 +177,21 @@ requirejs(['./WorldWindShim',
                 var img = document.getElementById("myImg");
                 var modalImg = document.getElementById("img01");
                 var captionText = document.getElementById("caption");
-                img.onclick = function(){
-                    modal.style.display = "block";
-                    modalImg.src = this.src;
-                    captionText.innerHTML = this.alt;
-                }
+
                 // Add the placemarks layer to the WorldWindow's layer list.
                 wwd.addLayer(placemarkLayer);
                 wwd.addLayer(placemarkLayer1);
 
-
-                // Get the <span> element that closes the modal
-                var span = document.getElementsByClassName("close")[0];
-
-                // When the user clicks on <span> (x), close the modal
-                span.onclick = function() {
+                // Get the <span1> element that closes the modal
+                var span1 = document.getElementsByClassName("close")[0];
+                // When the user clicks on <span2> (x), close the modal
+                span1.onclick = function() {
                     modal.style.display = "none";
-                }
 
 
                 // Now set up to handle picking.
 
-                var highlightedItems = [];
+                var highlightedItems = [placemark1];
 
                 // The common pick-handling function.
                 var handlePick = function (o) {
@@ -266,15 +206,31 @@ requirejs(['./WorldWindShim',
                     for (var h = 0; h < highlightedItems.length; h++) {
                         highlightedItems[h].highlighted = false;
                     }
-                    highlightedItems = [];
+                    highlightedItems = [placemark1];
 
                     // Perform the pick. Must first convert from window coordinates to canvas coordinates, which are
                     // relative to the upper left corner of the canvas rather than the upper left corner of the page.
-                    var pickList = wwd.pick(wwd.canvasCoordinates(x, y));
+                    var pickList = wwd.pick(wwd.canvasCoordinates(x,y));
+                    console.log(pickList.objects);
                     if (pickList.objects.length > 0) {
                         redrawRequired = true;
                     }
 
+                    // Get the image and insert it inside the modal - use its "alt" text as a caption
+                    var img = document.getElementById("myImg");
+                    var modalImg = document.getElementById("img01");
+                    var captionText = document.getElementById("caption");
+                    img.onclick = function(){
+                        modal.style.display = "block";
+                        modalImg.src = this.src;
+                        captionText.innerHTML = this.alt;
+                    }
+                    // Add the placemarks layer to the WorldWindow's layer list.
+                    wwd.addLayer(placemarkLayer);
+                    wwd.addLayer(placemarkLayer1);
+
+                    // Get the <span> element that closes the modal
+                    var span = document.getElementsByClassName("close")[0];
                     // Highlight the items picked by simply setting their highlight flag to true.
                     if (pickList.objects.length > 0) {
                         for (var p = 0; p < pickList.objects.length; p++) {
@@ -289,6 +245,13 @@ requirejs(['./WorldWindShim',
                             // or is merely picking the placemark as a whole.
                             if (pickList.objects[p].labelPicked) {
                                 console.log("Label picked");
+                                img = function(){
+                                    modal.style.display = "block";
+                                    modalImg.src = this.src;
+                                    captionText.innerHTML = this.alt;
+                                }
+
+                                }
                             }
                         }
                     }
@@ -300,7 +263,7 @@ requirejs(['./WorldWindShim',
                 };
 
                 // Listen for mouse moves and highlight the placemarks that the cursor rolls over.
-                wwd.addEventListener("mousemove", handlePick);
+                wwd.addEventListener("click", handlePick);
 
                 // Listen for taps on mobile devices and highlight the placemarks that the user taps.
                 var tapRecognizer = new WorldWind.TapRecognizer(wwd, handlePick);
@@ -308,137 +271,3 @@ requirejs(['./WorldWindShim',
                 // Create a layer manager for controlling layer visibility.
                 var layerManager = new LayerManager(wwd);
             });
-
-        var pinLibrary = WorldWind.configuration.baseUrl + "images/pushpins/",
-            // location of the image files
-            placemark,
-            placemarkAttributes = new WorldWind.PlacemarkAttributes(null),
-            highlightAttributes,
-            placemarkLayer = new WorldWind.RenderableLayer("Placemarks"),
-            latitude = 41.52110086382958,
-            longitude =  -74.44881797293047;
-
-        // Set up the common placemark attributes.
-        placemarkAttributes.imageScale = 1;
-        placemarkAttributes.imageOffset = new WorldWind.Offset(
-            WorldWind.OFFSET_FRACTION, 0.3,
-            WorldWind.OFFSET_FRACTION, 0.0);
-        placemarkAttributes.imageColor = WorldWind.Color.WHITE;
-        placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-            WorldWind.OFFSET_FRACTION, 0.5,
-            WorldWind.OFFSET_FRACTION, 1.0);
-        placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
-        placemarkAttributes.drawLeaderLine = true;
-        placemarkAttributes.leaderLineAttributes.outlineColor = WorldWind.Color.RED;
-
-        // For each placemark image, create a placemark with a label.
-        for (var i = 0, len = images.length; i < len; i++) {
-
-            // Create the placemark and its label.
-            placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude, 1e2), true, null);
-            placemark.label = "My house ".toString() + "\n"
-                + "Lat " + placemark.position.latitude.toPrecision(4).toString() + "\n"
-                + "Lon " + placemark.position.longitude.toPrecision(5).toString();
-            placemark.altitudeMode = WorldWind.RELATIVE_TO_GROUND;
-            placemarkAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
-            placemarkAttributes.imageSource = pinLibrary;
-            placemark.attributes = placemarkAttributes;
-
-            // Create the placemark attributes for this placemark. Note that the attributes differ only by their
-            // image URL.
-            placemarkAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
-            placemarkAttributes.imageSource = pinLibrary + images1[i];
-            placemark.attributes = placemarkAttributes;
-
-            // Create the highlight attributes for this placemark. Note that the normal attributes are specified as
-            // the default highlight attributes so that all properties are identical except the image scale. You could
-            // instead vary the color, image, or other property to control the highlight representation.
-            highlightAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
-            highlightAttributes.imageScale = 1.2;
-            placemark.highlightAttributes = highlightAttributes;
-
-            // Add the placemark to the layer.
-            placemarkLayer.addRenderable(placemark);
-        }
-        // Add the placemarks layer to the WorldWindow's layer list.
-        wwd.addLayer(placemarkLayer);
-
-        // Now set up to handle picking.
-
-        var highlightedItems = [];
-
-        // The common pick-handling function.
-        var handlePick = function (o) {
-            // The input argument is either an Event or a TapRecognizer. Both have the same properties for determining
-            // the mouse or tap location.
-            var x = o.clientX,
-                y = o.clientY;
-
-            var redrawRequired = highlightedItems.length > 0; // must redraw if we de-highlight previously picked items
-
-            // De-highlight any previously highlighted placemarks.
-            for (var h = 0; h < highlightedItems.length; h++) {
-                highlightedItems[h].highlighted = false;
-            }
-            highlightedItems = [];
-
-            // Perform the pick. Must first convert from window coordinates to canvas coordinates, which are
-            // relative to the upper left corner of the canvas rather than the upper left corner of the page.
-            var pickList = wwd.pick(wwd.canvasCoordinates(x, y));
-            if (pickList.objects.length > 0) {
-                redrawRequired = true;
-            }
-
-            // Highlight the items picked by simply setting their highlight flag to true.
-            if (pickList.objects.length > 0) {
-                for (var p = 0; p < pickList.objects.length; p++) {
-                    pickList.objects[p].userObject.highlighted = true;
-
-                    // Keep track of highlighted items in order to de-highlight them later.
-                    highlightedItems.push(pickList.objects[p].userObject);
-
-                    // Detect whether the placemark's label was picked. If so, the "labelPicked" property is true.
-                    // If instead the user picked the placemark's image, the "labelPicked" property is false.
-                    // Applications might use this information to determine whether the user wants to edit the label
-                    // or is merely picking the placemark as a whole.
-                    if (pickList.objects[p].labelPicked) {
-
-                    }
-                }
-            }
-
-            // Get the modal
-            var modal = document.getElementById("myModal");
-
-            // Get the image and insert it inside the modal - use its "alt" text as a caption
-            var img = document.getElementById("myImg");
-            var modalImg = document.getElementById("img01");
-            var captionText = document.getElementById("caption");
-            // placemark.onclick = function(){
-            //     modal.style.display = "block";
-            //     modalImg.src = this.src;
-            //     captionText.innerHTML = this.alt;
-            // }
-
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
-
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-                modal.style.display = "none";
-            }
-            // Update the window if we changed anything.
-            if (redrawRequired) {
-                wwd.redraw(); // redraw to make the highlighting changes take effect on the screen
-            }
-        };
-
-        // Listen for mouse moves and highlight the placemarks that the cursor rolls over.
-        wwd.addEventListener("mousemove", handlePick);
-
-        // Listen for taps on mobile devices and highlight the placemarks that the user taps.
-        var tapRecognizer = new WorldWind.TapRecognizer(wwd, handlePick);
-
-        // Create a layer manager for controlling layer visibility.
-        var layerManager = new LayerManager(wwd);
-    });
